4c6d75d857adccc78bbc2728ecb372bb6d00d73d
jhonatan estevam <jhonatan229@outlook.com>
2020-01-27 12:58:27 -0300

    Starting to implement Bpard and Pieces

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,15 +1,13 @@
 package application;
 
+import boardGame.Board;
 import boardGame.Position;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Hello Chess!!");
-
-		Position po = new Position(6, 1);
-		System.out.println(po);
+		Board board = new Board(8, 8);
 	}
 
 }
