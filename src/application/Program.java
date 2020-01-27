bd8f517bb056ab295ce644b9ebc07729f7124226
jhonatan estevam <jhonatan229@outlook.com>
2020-01-27 12:44:55 -0300

    first class: Possition

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,11 +1,15 @@
 package application;
 
+import boardGame.Position;
+
 public class Program {
 
 	public static void main(String[] args) {
 		
 		System.out.println("Hello Chess!!");
 
+		Position po = new Position(6, 1);
+		System.out.println(po);
 	}
 
 }
